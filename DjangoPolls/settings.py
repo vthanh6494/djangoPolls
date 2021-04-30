ee4100f593c465560e4dc4f80fd30a5ccc529186
Anh Vo <v.anhvo@vinbrain.net>
2021-04-30 15:23:23 +0700

    update requirement.txt

--- DjangoPolls/settings.py
+++ DjangoPolls/settings.py
@@ -151,3 +151,6 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
